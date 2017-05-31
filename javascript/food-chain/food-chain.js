e7728124349d2153de386c869b05eb8475928737
Deliagwath <PPL3000@Gmail.com>
2017-05-31 16:36:18 +0700

    FoodChain Skeleton File

---
+++ javascript/food-chain/food-chain.js
@@ -0,0 +1,11 @@
+var FoodChain = function() {}
+
+FoodChain.prototype.verse = function(verse_num) {
+
+}
+
+FoodChain.prototype.verses = function(verse_start, verse_end) {
+
+}
+
+module.exports = FoodChain;
