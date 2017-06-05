a9ff003cde27ab24f453ac09b13ecb021da8e5a1
Deliagwath <PPL3000@Gmail.com>
2017-06-05 13:33:35 +0700

    Circular Buffer Implementation

--- javascript/circular-buffer/circular-buffer.js
+++ javascript/circular-buffer/circular-buffer.js
@@ -1,3 +1,7 @@
+var handler = function(size) {
+    return new circularBuffer(size);
+}
+
 var circularBuffer = function(size) {
     this.buffer = [];
     this.size = size;
@@ -5,20 +9,34 @@ var circularBuffer = function(size) {
 
 circularBuffer.prototype.write = function(val) {
     if (this.buffer.length == this.size) { bufferFullException(); }
+    if (val === undefined || val == null) { return; }
+    this.buffer.push(val);
+}
+
+circularBuffer.prototype.forceWrite = function(val) {
+    if (this.buffer.length == this.size) { this.buffer.shift(); }
+    this.buffer.push(val);
 }
 
 circularBuffer.prototype.read = function() {
     if (!this.buffer.length) { bufferEmptyException(); }
+    return this.buffer.shift();
+}
+
+circularBuffer.prototype.clear = function() {
+    this.buffer = [];
 }
 
 var bufferEmptyException = function() {
-    throw "Buffer is empty";
+    return new Error("BufferEmptyException");
 }
 
 var bufferFullException = function() {
-    throw "Buffer is full";
+    return new Error("BufferFullException");
 }
 
-module.exports.circularBuffer = circularBuffer;
-module.exports.bufferEmptyException = bufferEmptyException;
-module.exports.bufferFullException = bufferFullException;
+module.exports = {
+    circularBuffer: handler,
+    bufferEmptyException: bufferEmptyException,
+    bufferFullException: bufferEmptyException,
+}
