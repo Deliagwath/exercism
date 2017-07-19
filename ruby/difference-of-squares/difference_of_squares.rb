4e3dad9f7835c7f6387f796c290218b61cfd3ccd
Deliagwath <PPL3000@Gmail.com>
2017-07-19 11:25:54 +0700

    Difference of Squares Implementation #3

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,16 +1,15 @@
 class Squares
-    def self.new(n)
-        @num = n.times.map { |nm| nm + 1 }
-        self
+    def initialize(n)
+        @num = (1..n).to_a
     end
-    def self.difference
-        self.square_of_sum - self.sum_of_squares
+    def difference
+        square_of_sum - sum_of_squares
     end
-    def self.square_of_sum
-        @num.reduce(:+) ** 2
+    def square_of_sum
+        @num.sum ** 2
     end
-    def self.sum_of_squares
-        @num.map { |n| n ** 2 }.reduce(:+)
+    def sum_of_squares
+        @num.map { |n| n ** 2 }.sum
     end
 end
 module BookKeeping
