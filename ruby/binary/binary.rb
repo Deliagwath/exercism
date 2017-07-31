95e256067f57a5ce63e524fb718e62801c8fe036
Deliagwath <PPL3000@Gmail.com>
2017-07-31 12:11:29 +0700

    Binary Fix

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -2,7 +2,7 @@ class Binary
     def self.to_decimal(bin_str)
         raise ArgumentError.new("Not a Binary String") if bin_str =~ /[^01]/
         bin_str.chars.reverse.each_with_index.reject { |d, i|
-            d.to_i == 0
+            d == "0"
         }.map { |d, i|
             2 ** i
         }.reduce(0, :+)
