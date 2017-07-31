e9617b54b12b42a675b00534d4d65f7afec8d1a1
Deliagwath <PPL3000@Gmail.com>
2017-07-31 16:47:33 +0700

    Grade School Implementation

---
+++ ruby/grade-school/grade_school.rb
@@ -0,0 +1,20 @@
+class School
+    def initialize
+        @grades = Hash.new { |hash, grade| hash[grade] = [] }
+    end
+    def add(student, grade)
+        @grades[grade] << student
+        @grades[grade].sort!
+    end
+    def students(grade_num)
+        @grades[grade_num].sort
+    end
+    def students_by_grade
+        @grades.sort.map { |grades, students|
+            { :grade => grades, :students => students }
+        }
+    end
+end
+module BookKeeping
+    VERSION = 3
+end
