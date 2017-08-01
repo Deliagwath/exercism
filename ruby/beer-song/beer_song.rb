bff53bdec94fcd2c24b019fa2c640148cadf611e
Deliagwath <PPL3000@Gmail.com>
2017-08-01 15:02:53 +0700

    Beer Song Skeleton

---
+++ ruby/beer-song/beer_song.rb
@@ -0,0 +1,15 @@
+class BeerSong
+    def initialize
+    end
+    def verse(verse_no)
+        <<-TEXT
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+TEXT
+    end
+    def verses(verse_start, verse_end)
+    end
+end
+module BookKeeping
+    VERSION = 3
+end
