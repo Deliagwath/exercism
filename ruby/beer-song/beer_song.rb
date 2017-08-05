e2d93d667d31162f7c01e35efb70458845cccd96
Pira Limpiti <Deliagwath@localhost.localdomain>
2017-08-05 14:15:41 +0700

    Beer Song Verse Implementation

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -2,10 +2,19 @@ class BeerSong
     def initialize
     end
     def verse(verse_no)
-        <<-TEXT
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
-TEXT
+        s1(verse_no) + s2(verse_no)
+    end
+    def s1(verse_no)
+        num_bottles = verse_no > 0 ? verse_no : "No more"
+        plural = verse_no == 1 ? "" : "s"
+        "#{num_bottles} bottle#{plural} of beer on the wall, " +
+        "#{num_bottles.downcase} bottle#{plural} of beer.\n"
+    end
+    def s2(verse_no)
+        p1 = verse_no > 0 ? "Take it down and pass it around" : "Go to the store and buy some more"
+        num_bottles = verse_no - 1 > 0 ? verse_no : "no more"
+        plural = verse_no - 1 == 1 ? "" : "s"
+        "#{p1}, #{num_bottles} bottle#{plural} of beer on the wall.\n"
     end
     def verses(verse_start, verse_end)
     end
