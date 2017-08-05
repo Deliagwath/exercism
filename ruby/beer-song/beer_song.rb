0301274db06ca155f9ffc4954e7accd952f715be
Pira Limpiti <Deliagwath@localhost.localdomain>
2017-08-05 14:46:42 +0700

    Beer Song Implementation

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -8,15 +8,18 @@ class BeerSong
         num_bottles = verse_no > 0 ? verse_no : "No more"
         plural = verse_no == 1 ? "" : "s"
         "#{num_bottles} bottle#{plural} of beer on the wall, " +
-        "#{num_bottles.downcase} bottle#{plural} of beer.\n"
+        "#{num_bottles.to_s.downcase} bottle#{plural} of beer.\n"
     end
     def s2(verse_no)
-        p1 = verse_no > 0 ? "Take it down and pass it around" : "Go to the store and buy some more"
-        num_bottles = verse_no - 1 > 0 ? verse_no : "no more"
+        p1 = verse_no > 0 ? "Take #{verse_no == 1 ? "it" : "one"} down and pass it around" : "Go to the store and buy some more"
+        num_bottles = verse_no - 1 == 0 ? "no more" : (verse_no - 1 == -1 ? 99 : verse_no - 1)
         plural = verse_no - 1 == 1 ? "" : "s"
         "#{p1}, #{num_bottles} bottle#{plural} of beer on the wall.\n"
     end
     def verses(verse_start, verse_end)
+        verse_start.downto(verse_end)
+            .map { |index| verse(index) }
+            .join("\n")
     end
 end
 module BookKeeping
